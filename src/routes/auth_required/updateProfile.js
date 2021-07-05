379d091a9c8875fb7dfb20a5b64ce44f8be2e024
Maxym Menzheha <masimfyd@gmail.com>
2021-07-05 19:46:39 +0300

    added update password, update functions validations

--- src/routes/auth_required/updateProfile.js
+++ src/routes/auth_required/updateProfile.js
@@ -1,29 +1,64 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 const User = require('../../model/User');
 const verify = require('../../token/verifyToken');
+const {nameValidation, emailValidation, passwordValidation} = require('../../validation/validaton'); 
 
 
 
 router.post('/update-profile/username', verify, async (req, res) => {
+
+    const { error } = nameValidation(req.body);
+    if(error) {
+        return res.status(400).send(JSON.stringify({'error': error.details[0].message}));
+    }
 
     const usernameExist = await User.findOne({ name: req.body.name });
     if(usernameExist) {
         return res.status(400).send(JSON.stringify({'error': 'Username already exist'}));
     }
 
-    const user = await User.findOneAndUpdate({ _id: req.body.userId }, {name: req.body.name});
+    const user = await User.findOneAndUpdate({ _id: req.user.id }, {name: req.body.name});
 
     res.status(200).send(JSON.stringify({'message': 'OK'}));
 })
 
 router.post('/update-profile/email', verify, async (req, res) => {
 
+    const { error } = emailValidation(req.body);
+    if(error) {
+        return res.status(400).send(JSON.stringify({'error': error.details[0].message}));
+    }
+
     const emailExist = await User.findOne({ email: req.body.email });
     if(emailExist) {
         return res.status(400).send(JSON.stringify({'error': 'Email already exist'}));
     }
 
-    const user = await User.findOneAndUpdate({ _id: req.body.userId }, {email: req.body.email});
+    const user = await User.findOneAndUpdate({ _id: req.user.id }, {email: req.body.email});
+
+    res.status(200).send(JSON.stringify({'message': 'OK'}));
+})
+
+router.post('/update-profile/password', verify, async (req, res) => {
+
+    try {
+        // check if password is the actual user password ???
+        // add password validation
+
+        const { error } = passwordValidation(req.body);
+        if(error) {
+            return res.status(400).send(JSON.stringify({'error': error.details[0].message}));
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+        const user = await User.findOneAndUpdate({ _id: req.user.id }, {password: hashedPassword});
+    } catch (err) {
+        console.log(err);
+    }
+    
 
     res.status(200).send(JSON.stringify({'message': 'OK'}));
 })
--- src/validation/validaton.js
+++ src/validation/validaton.js
@@ -21,5 +21,32 @@ const loginValidation = (data) => {
     return schema.validate(data);
 }
 
+const nameValidation = (data) => {
+    const schema = Joi.object({
+        name: Joi.string().min(5).required()
+    })
+
+    return schema.validate(data);
+}
+
+const emailValidation = (data) => {
+    const schema = Joi.object({
+        email: Joi.string().min(6).required().email()
+    })
+
+    return schema.validate(data);
+}
+
+const passwordValidation = (data) => {
+    const schema = Joi.object({
+        password: Joi.string().min(6).required()
+    })
+
+    return schema.validate(data);
+}
+
 module.exports.registerValidation = registerValidation;
 module.exports.loginValidation = loginValidation;
+module.exports.nameValidation = nameValidation;
+module.exports.emailValidation = emailValidation;
+module.exports.passwordValidation = passwordValidation;
