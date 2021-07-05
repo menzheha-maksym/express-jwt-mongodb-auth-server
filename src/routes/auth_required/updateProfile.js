8eead5275f71ab50ef1fc7b89f7c2aac9f05ae73
Maxym Menzheha <masimfyd@gmail.com>
2021-07-05 18:37:54 +0300

    added update email function

--- src/routes/auth_required/updateProfile.js
+++ src/routes/auth_required/updateProfile.js
@@ -8,13 +8,22 @@ router.post('/update-profile/username', verify, async (req, res) => {
 
     const usernameExist = await User.findOne({ name: req.body.name });
     if(usernameExist) {
-        console.log("exist " + req.body.name);
         return res.status(400).send(JSON.stringify({'error': 'Username already exist'}));
     }
 
-    console.log("not exist " + req.body.name);
-
     const user = await User.findOneAndUpdate({ _id: req.body.userId }, {name: req.body.name});
+
+    res.status(200).send(JSON.stringify({'message': 'OK'}));
+})
+
+router.post('/update-profile/email', verify, async (req, res) => {
+
+    const emailExist = await User.findOne({ email: req.body.email });
+    if(emailExist) {
+        return res.status(400).send(JSON.stringify({'error': 'Email already exist'}));
+    }
+
+    const user = await User.findOneAndUpdate({ _id: req.body.userId }, {email: req.body.email});
 
     res.status(200).send(JSON.stringify({'message': 'OK'}));
 })
